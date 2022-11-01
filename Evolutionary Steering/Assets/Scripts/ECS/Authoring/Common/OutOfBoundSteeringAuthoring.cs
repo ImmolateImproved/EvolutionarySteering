b6f12c48989a31defc9448b7b8fc3c97e2d814f9
Immolate <FONTOoMas@gmail.com>
2022-11-01 04:54:15 +0300

    WorldBounds refactoring.
    WorldBounds now can take bounds from position factory.

--- Evolutionary Steering/Assets/Scripts/ECS/Authoring/Common/OutOfBoundSteeringAuthoring.cs
+++ Evolutionary Steering/Assets/Scripts/ECS/Authoring/Common/OutOfBoundSteeringAuthoring.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class OutOfBoundSteeringAuthoring : MonoBehaviour
 {
+    public PositionFactoryData positionFactory;
+
     public Bounds bounds;
     public float steeringForce;
 
@@ -11,9 +13,21 @@ public class OutOfBoundSteeringAuthoring : MonoBehaviour
     {
         public override void Bake(OutOfBoundSteeringAuthoring authoring)
         {
+            var posFactory = authoring.positionFactory;
+
+            var bounds = authoring.bounds;
+
+            if (posFactory)
+            {
+                DependsOn(posFactory.transform);
+
+                bounds.center = posFactory.transform.position;
+                bounds.extents = posFactory.bounds;
+            }
+
             AddComponent(new OutOfBoundSteering
             {
-                squareBounds = authoring.bounds,
+                squareBounds = bounds,
                 steeringForce = authoring.steeringForce
 
             });
